270ca54044c12a20abef49de52d29d04f69256d3
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-16 17:34:08 -0400

    Initial Commit

---
+++ DynamicallyAllocMem2DArray/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DynamicallyAllocMem2DArray
+//
+//  Created by Matt Ahmad on 2022-10-16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
